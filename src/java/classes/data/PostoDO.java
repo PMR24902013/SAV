82dd6f0c6e95730759d74df1fa75d2aa9302e9ba
hidden <hidden@172.21.123.226>
2013-11-05 21:22:27 -0200

    login terminado indo para s menus dos respectivos usuarios

---
+++ src/java/classes/data/PostoDO.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.data;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author hidden
+ */
+public class PostoDO {
+    
+     private int _id;
+     private int _usuariosid;
+    private String _nome;
+    private String _endereco;
+     private String _telefone;
+      private String _horarioDeFuncionamento;
+       private String _responsavel;
+    private String _documentoDeLicensiamento;
+    
+    
+    public int getID(){
+        return _id;
+    }
+    public int getUsuariosID(){
+        return _usuariosid;
+    }
+    
+    public String getNome(){
+        return _nome;
+    }
+    
+    public String getEndereco(){
+        return _endereco;
+    }
+    
+    public String getTelefone(){
+        return _telefone;
+    }
+     public String getHorario(){
+        return _horarioDeFuncionamento;
+    }
+      public String getResponsavel(){
+        return _responsavel;
+    }
+       public String getDocumento(){
+        return _documentoDeLicensiamento;
+    }
+    
+   public void setID(int id){
+        _id=id;
+    }
+    public void setUsuariosID(int id){
+        _usuariosid=id;
+    }
+    
+    public void setNome(String nome){
+         _nome=nome;
+    }
+    
+    public void setEndereco(String end){
+         _endereco=end;
+    }
+    
+    public void setTelefone(String tel){
+        _telefone=tel;
+    }
+     public void setHorario(String hor){
+        _horarioDeFuncionamento=hor;
+    }
+      public void setResponsavel(String resp){
+        _responsavel=resp;
+    }
+       public void setDocumento(String doc){
+       _documentoDeLicensiamento=doc;
+    }
+    
+}
---
+++ src/java/classes/data/PostoData.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.data;
+
+import classes.utils.Transacao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+/**
+ *
+ * @author hidden
+ */
+public class PostoData {
+     public void incluir(PostoDO posto, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "insert into Posto (Usuarios_ID, Nome, Endereco, Telefone, Horario_de_Funcionamento, Responsavel, Documento_de_Licenciamento) VALUES (?,?,?,?,?,?,?)";
+     PreparedStatement ps = con.prepareStatement(sql);
+     
+     ps.setInt(1, posto.getUsuariosID());
+     ps.setString(2, posto.getNome());
+     ps.setString(3, posto.getEndereco());
+     ps.setString(4, posto.getTelefone());
+     ps.setString(5, posto.getHorario());
+     ps.setString(6, posto.getResponsavel());
+     ps.setString(7, posto.getDocumento());
+     int result = ps.executeUpdate();
+  }
+
+  public void excluir(PostoDO posto, Transacao tr) throws Exception {
+     excluir(posto.getID(), tr);
+  } // excluir
+
+  public void excluir (int idobj, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "delete from Posto where id=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     String id = ""+idobj;
+     ps.setString(1, id);
+     int result = ps.executeUpdate();
+  } // excluir 
+
+  public void atualizar(PostoDO posto, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "update Posto set Usuarios_ID=?, Nome=?, Endereco=?, Telefone=?, Horario_de_Funcionamento=?, Responsavel=?, Documento_de_Licenciamento=? where id=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setInt(1, posto.getID());
+     ps.setInt(2, posto.getUsuariosID());
+     ps.setString(3, posto.getNome());
+     ps.setString(4, posto.getEndereco());
+     ps.setString(5, posto.getTelefone());
+     ps.setString(6, posto.getHorario());
+     ps.setString(7, posto.getResponsavel());
+     ps.setString(8, posto.getDocumento());
+     int result = ps.executeUpdate();
+  } // atualizar
+
+  public PostoDO buscar(int idobj, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "select * from Posto where  id=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setInt(1, idobj);
+     ResultSet rs = ps.executeQuery();
+     rs.next();
+     PostoDO posto = new PostoDO();
+     posto.setID (rs.getInt("id"));
+      posto.setUsuariosID (rs.getInt("Usuarios_ID"));
+     posto.setNome (rs.getString("nome"));
+     posto.setEndereco (rs.getString("endereco"));
+     posto.setTelefone (rs.getString("telefone"));
+     posto.setHorario (rs.getString("Horario_de_Funcionamento"));
+     posto.setResponsavel (rs.getString("Responsavel"));
+     posto.setDocumento(rs.getString("Documento_de_Licenciamento"));
+     return posto;
+  } // buscar
+
+  public Vector pesquisarPorLogin(String login, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "select * from Posto where login like ?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setString(1, login);
+     ResultSet rs = ps.executeQuery();
+     System.out.println("query executada");
+     Vector postos = new Vector();
+     while (rs.next()) {
+        PostoDO posto = new PostoDO();
+         posto.setID (rs.getInt("id"));
+      posto.setUsuariosID (rs.getInt("Usuarios_ID"));
+     posto.setNome (rs.getString("nome"));
+     posto.setEndereco (rs.getString("endereco"));
+     posto.setTelefone (rs.getString("telefone"));
+     posto.setHorario (rs.getString("Horario_de_Funcionamento"));
+     posto.setResponsavel (rs.getString("Responsavel"));
+     posto.setDocumento(rs.getString("Documento_de_Licenciamento"));
+        postos.add(posto);
+     }
+     return postos;
+  } // pesquisarPorLogin
+    
+}
