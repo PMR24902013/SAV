99579b7de1e7ad8bed3c3aa9786ced8421c7e4c7
Rodrigo <Rodrigo@Rolando_note>
2013-11-25 23:53:48 -0200

    Caso de uso de alterar preços...

--- src/java/classes/data/PrecosData.java
+++ src/java/classes/data/PrecosData.java
@@ -31,4 +31,19 @@ public class PrecosData {
      return preco;
   } // buscar
   
+   public void atualizar(PrecosDO preco, Transacao tr) throws Exception {
+        Connection con = tr.obterConexao();
+        String sql = "update Precos set Veiculo_ID=?, Preco1=?, Preco2=? ,Preco3=?, Preco4=?, Preco5=?, Preco6=? where id=?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, preco.getModeloId());
+        ps.setFloat(2, preco.getPreco1());
+        ps.setFloat(3, preco.getPreco2());
+        ps.setFloat(4, preco.getPreco3());
+        ps.setFloat(5, preco.getPreco4());
+        ps.setFloat(6, preco.getPreco5());
+        ps.setFloat(7, preco.getPreco6());
+        ps.setInt(8, preco.getId());
+        int result = ps.executeUpdate();
+    }
+  
 }
